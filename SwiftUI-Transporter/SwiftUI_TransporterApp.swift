9da246956894eccdca9d2c3a452ab202a29270c7
Ruaidhri O'Neill <Ruaidhri2006@gmail.com>
2021-08-17 22:43:47 +0100

    Initial Commit

---
+++ SwiftUI-Transporter/SwiftUI_TransporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TransporterApp.swift
+//  SwiftUI-Transporter
+//
+//  Created by Ruaidhrí O'Neill on 17/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TransporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
